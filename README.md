c284d623b3af471fd24771cf9d1a4c7118b6f62a
Bumerrrang <andrei.lunin.0@gmail.com>
2024-10-08 08:41:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# book-social-network-api
+…or create a new repository on the command line
+
+echo "# book-social-network-api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bumerrrang/book-social-network-api.git
+git push -u origin main
+
---
+++ src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -0,0 +1,15 @@
+package com.alibou.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// https://github.com/ali-bouali/book-social-network/tree/main/book-network
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
